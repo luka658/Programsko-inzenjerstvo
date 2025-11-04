48571cd0559cb9fec43e9c0277d3997323b57688
Krešimir Kantolić <kk55892@fer.hr>
2025-11-04 22:35:08 +0100

    dodana label komponenta

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label"; 
+
 
 export default function Home() {
 
@@ -7,6 +9,8 @@ export default function Home() {
       <div className="flex flex-row">
         Hello World!
         <Button>Button</Button>
+        <Label> in paris
+        </Label>
       </div>
     </>
   );
