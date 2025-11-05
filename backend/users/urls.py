343a334f02604e86ac72b3fa18f52bb8e09c3ef6
renatodolic <rd55841@fer.hr>
2025-11-05 23:35:59 +0100

    Logout, delete, reset password
    dodani api endpointi za logout, delete account, reset password u users/views.py
    - dodani i urlovi za to u users/urls.py
    -u backend/settings.py dodan FRONTEND_URL

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,16 @@
 
 
 from django.urls import path
-from . import views
+from .views import RegisterView, LoginView, requestPasswordResetView, resetPasswordConfirmView, logoutView, deleteUserView, caretakers
 
 
 urlpatterns = [
-    path('caretakers/', views.caretakers, name='caretakers')
+    path('login', LoginView.as_view(), name="login"),
+    path('register', RegisterView.as_view(), name="register"),
+    path('logout/', logoutView, name="logout-user"),
+    path('delete/', deleteUserView, name="delete-user"),
+    path('forgot-password/', requestPasswordResetView, name="forgot-password"),
+    path('reset-password/<uidb64>/<token>/', resetPasswordConfirmView, name="reset-password"),
+    path('caretakers/', caretakers, name='caretakers'),
+
 ]
