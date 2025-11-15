ab6f6c94a904501e84c4f484cecfe93dd562f82b
Luka-Bubnjevic <134085001+luka658@users.noreply.github.com>
2025-11-15 16:53:10 +0100

    basic landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-row">
-        Hello World!
+    <div>
+      <div className="flex flex-col items-center">
+        <h1 className="font-semibold text-3xl mb-5">Welcome to Carefree!</h1>
+        
+        <div className="space-x-2">
+          <Button asChild><Link href="/accounts/login">Login</Link></Button>
+          <Button asChild><Link href="/accounts/signup">Sign Up</Link></Button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
